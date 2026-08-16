1e9b581480bd0df83ee0634aeff64d2ccb8eb266
Fenilrathod002 <d26it117@charusat.edu.in>
2026-08-16 23:18:06 +0530

    CSS Added

---
+++ StudentHub/js/script.js
@@ -0,0 +1,42 @@
+console.log("StudentHub Javascript Loaded Successfully.");
+console.log("Welcome To The StudentHub.");
+console.log("Practical-4 Javascript");
+
+
+
+let studentname = "Fenil";
+let course = "Information Technology";
+let semester = "3";
+console.log(studentname);
+console.log(course);
+console.log(semester);
+
+
+let college = "CHARUSAT";
+let year = "2026";
+let isStudent = True;
+console.log(college);
+console.log(year);
+console.log(isStudent);
+
+
+function welcomepage(){
+    console.log("Welcome To The StudentHub.")
+}
+
+welcomepage();
+
+function student(name, mycourse){
+    console.log("Hii, I Am " + name + " I Am In " + mycourse + " Departement.");
+}
+
+student("Fenil Rathod","IT");
+
+
+
+let changeHeading = document.getElementById("heading");
+// changeHeading.onclick = ;
+
+
+
+
